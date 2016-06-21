61dd22d87fea3a92a302a9ccded8abc2338e716b
Eric Kramer <cosmocracy@yahoo.com>
2016-06-21 16:57:43 -0400

    Initial implementation
    Pins 9, 10, 11 glow up at startup and remain at 100%

--- uno-sketch1/uno-sketch1.ino
+++ uno-sketch1/uno-sketch1.ino
@@ -3,10 +3,16 @@ void setup() {
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
 
+  ignite(9, 5);
+  ignite(10, 5);
+  ignite(11, 5);
+}
+
+void ignite(int pin, int delay_ms) {
   for( int i = 0; i < 255; i++ ) {
-    analogWrite(9, i);
-    delay(30);
-  }
+    analogWrite(pin, i);
+    delay(delay_ms);
+  }  
 }
 
 void loop() {
